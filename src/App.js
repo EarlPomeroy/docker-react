0f3e6daca80e7daf34d83f07019bc71fd9f88f0b
Earl Pomeroy <earl.pomeroy42@gmail.com>
2018-12-24 01:14:45 -0700

    Modified p text.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Bitches say 'woop, woop'!
+            Working on AWS!
           </p>
           <a
             className="App-link"
